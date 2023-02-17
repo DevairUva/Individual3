89c2ae928ab25b683723c92d86e38761e61fe256
DEVair Martins <83782674+DevairUva@users.noreply.github.com>
2023-02-17 19:40:52 -0300

    Atualização imagens pedido
    Atualização imagens pedido e link comanda

--- grupo3/src/components/carrinho/Tabela.jsx
+++ grupo3/src/components/carrinho/Tabela.jsx
@@ -78,7 +78,7 @@ function Tabela({ id }) {
         setShow(false)
         setShow2(true)
         setTimeout(function () {
-          window.location.href = "http://127.0.0.1:5173/#/comanda/";
+          window.location.href = "https://spaceburguersquad1.onrender.com/#/comanda";
         }, 4000);
       }
     } catch (e) {
